5354dc7ecdbceef6c0d1aaf6ffeb0312d9031dbc
Ferdidand79 <abelsangama@upeu.edu.pe>
2022-06-09 17:41:43 -0500

    creando contans

---
+++ lib/pages/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AppHome extends StatefulWidget {
+  const AppHome({Key? key}) : super(key: key);
+
+  @override
+  State<AppHome> createState() => _AppHomeState();
+}
+
+class _AppHomeState extends State<AppHome> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
