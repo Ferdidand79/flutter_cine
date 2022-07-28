5630fa6a17fc902b0d4505d5cb768fb5854856b7
Abel Fernando Sangama Oñate <abelsangama@upeu.edu.pe>
2022-07-28 14:24:07 -0500

    creando widget

---
+++ lib/pages/movies/movies_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MoviePage extends StatelessWidget {
+  const MoviePage({Key? key}) : super(key: key);
+
+  //final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
