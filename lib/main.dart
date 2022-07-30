093550141f35310b7646e7ae0bdc55ea198f8b65
Abel Fernando Sangama Oñate <abelsangama@upeu.edu.pe>
2022-07-29 23:01:58 -0500

    modificando titulo

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Cine02',
+      title: 'Flutter Cine01',
       theme: ThemeData(
         // This is the theme of your application.
         //
