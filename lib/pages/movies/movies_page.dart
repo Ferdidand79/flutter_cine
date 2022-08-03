b3fdbb4f89a79d84fb1df99de1f33f0fcc8d81a7
Abel Fernando Sangama Oñate <abelsangama@upeu.edu.pe>
2022-08-03 04:56:41 -0500

    terminando  movie page

--- lib/pages/movies/movies_page.dart
+++ lib/pages/movies/movies_page.dart
@@ -7,12 +7,26 @@ class MoviePage extends StatefulWidget {
   State<MoviePage> createState() => _MoviePageState();
 }
 
-class _MoviePageState extends State<MoviePage> {
-  //final Movie movie;
+class _MoviePageState extends State<MoviePage>
+    with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 3, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
     );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 }
