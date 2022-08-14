840fd224b0218c7f4927c8b00bfcaab03a04d062
Ferdidand79 <abelsangama@upeu.edu.pe>
2022-08-13 23:56:42 -0500

    subeindo

--- lib/pages/movies/widgets/movies_view.dart
+++ lib/pages/movies/widgets/movies_view.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -11,15 +13,38 @@ class MoviesView extends StatefulWidget {
 
 class _MoviesViewState extends State<MoviesView> {
   late final PageController _movieCardPageController;
-  late final PageController _movieDetailspageController;
+  late final PageController _movieDetailPageController;
 
   double _movieCardPage = 0.0;
-  double _movieDetailPage = 0.0;
+  double _movieDetailsPage = 0.0;
   int _movieCardIndex = 0;
   final _showMovieDetails = ValueNotifier(true);
 
   @override
+  Void initState() {
+    _movieCardPageController = PageController(viewportFraction: 0.77)
+      ..addListener(_movieCardPageListener);
+    _movieDetailPageController = PageController()
+      ..addListener(_movieDetailsPercentListener);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  _movieDetailsPercentListener() {
+    setState(() {
+      _movieCardPage = _movieCardPageController.page!;
+      ;
+      _movieCardIndex = _movieCardPageController.page!.round();
+    });
+  }
+
+  _movieCardPageListener() {
+    setState(() {
+      _movieDetailsPage = _movieDetailPageController.page!;
+    });
   }
 }
